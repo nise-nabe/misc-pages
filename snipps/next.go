22227aad3c017e3d9adc9fc2b95fc97adc767be2
nise_nabe <nise.nabe@gmail.com>
2012-03-10 01:33:13 +0900

    add snip to iterate space separated string

---
+++ snipps/next.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+        "index/suffixarray"
+        "log"
+        "regexp"
+)
+
+func main() {
+        str := "1234 2 3333 4 5  aaaaa"
+        next := func(str string) func() string {
+                reg, _ := regexp.Compile("\\S+")
+                is := suffixarray.New([]byte(str)).FindAllIndex(reg, -1)
+                return func() (result string) {
+                        if len(is) < 1 {
+                                return ""
+                        }
+                        result = str[is[0][0]:is[0][1]]
+                        is = is[1:]
+                        return
+                }
+        }(str)
+        for x := next(); x != ""; x = next() {
+                log.Println(x)
+        }
+}
