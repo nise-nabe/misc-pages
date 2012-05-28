ab5b668c94aa37719cce2624dd2d02849e271f64
nise_nabe <nise.nabe@gmail.com>
2012-05-28 10:06:07 +0900

    add link script

---
+++ web/js/nise_nabe.js
@@ -0,0 +1,14 @@
+$(function() {
+    "use strict";
+    var
+        li = [
+            {url: "http://d.hatena.ne.jp/nise_nabe"},
+            {url: "http://twitter.com/nise_nabe"},
+            {url: "http://github.com/nise-nabe"}
+        ],
+        template = "<li><a href=\"{{url}}\">{{url}}</li>",
+        content = $("#content");
+    li.forEach(function(elem) {
+        $("#links").append($.Mustache(template, elem));
+    });
+});
